a82f586f1919422065523b34e0ef0e32fbdc28bd
Nelson <villalobosnelson000@gmail.com>
2024-10-30 13:45:36 -0400

    10/30/24

--- v5/reviews_handler.py
+++ v5/reviews_handler.py
@@ -4,14 +4,17 @@ def get_first_review():
     try:
         with open(reviews, "r", encoding='utf-8') as file:
             lines = file.readlines()
-            if lines:
-                review_info = lines[0].strip()
-                with open(reviews, "w", encoding='utf-8') as updated_file:
-                    updated_file.writelines(lines[1:])
-                return review_info
-            else:
-                print("No more reviews in the file.")
-                return None
+            try:
+                if lines:
+                    review_info = lines[0].strip()
+                    with open(reviews, "w", encoding='utf-8') as updated_file:
+                        updated_file.writelines(lines[1:])
+                    return review_info
+                else:
+                    print("No more reviews in the file.")
+                    return None
+            except Exception as e:
+                print(f'Error: {e}')
     except FileNotFoundError:
         print(f"Error: {reviews} file not found.")
         return None
